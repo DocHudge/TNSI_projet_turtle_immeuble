cf6027189ab546e57f9e80927dc49db50e3f664a
Jules GAUNEE <jules.gaunee@gmail.com>
2026-09-11 09:36:22 +0200

    commit3

--- Immeuble.py
+++ Immeuble.py
@@ -1,1 +1,15 @@
-#début du projet là.
+from turtle import *
+
+taille = 130
+x = 0
+y = 0
+
+def carre(x, y, taille):
+    down()
+    goto(x, y)
+    pendown()
+    for _ in range(4):
+        forward(taille)
+        right(90)
+
+carre(x, y, taille)
