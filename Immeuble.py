338a44942a8abf4e6cf8538fa544483337086bfb
Jules GAUNEE <jules.gaunee@gmail.com>
2026-09-11 09:55:16 +0200

    commit 4

--- Immeuble.py
+++ Immeuble.py
@@ -1,17 +1,21 @@
 from turtle import *
 import random
+
 largeur = 140
 hauteur = 60
+
 x1 = 0
 y1 = 0
-goto (x1, y1)
+
 couleur1 = random.choice(["red", "blue", "green", "yellow", "purple", "orange"])
+
+color("black", couleur1)
+
 def etage0():
     pendown()
     begin_fill()
-    fillcolor(couleur1)
-
-    pencolor(couleur1)
+    
+    
     forward(largeur)
     left(90)
     forward(hauteur)
@@ -20,9 +24,15 @@ def etage0():
     left(90)    
     forward(hauteur)
     
+    end_fill()
 
+penup()
+goto(x1, y1)
 etage0()
-goto (x1, y1 + hauteur)
+
+penup()
+goto(x1, y1 + hauteur)
 setheading(0)
 etage0()
+
 done()
